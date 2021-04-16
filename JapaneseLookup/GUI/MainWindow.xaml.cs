c24876573ea3a92ee011f9c317c3c4d017ca9085
rampaa <rampaa@waifuland.com>
2021-04-16 20:49:58 +0300

    Implemented JMdictUpdater

--- JapaneseLookup/GUI/MainWindow.xaml.cs
+++ JapaneseLookup/GUI/MainWindow.xaml.cs
@@ -41,10 +41,10 @@ namespace JapaneseLookup.GUI
                 try
                 {
                     string text = Clipboard.GetText();
-                    Regex japaneseRegex = new Regex(@"[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]");
+                    Regex japaneseRegex = new(@"[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]");
                     if (japaneseRegex.IsMatch(text))
                     {
-                        backlog += text;
+                        backlog += text + "\n";
                         mainTextBox.Text = text + "\n";
                     }
                 }
