12566ad7575ecee262dd252419bcb051f8546735
rampaa <rampaa@waifuland.com>
2021-05-06 21:11:36 +0300

    Made textbox font customizable

--- JapaneseLookup/ConfigManager.cs
+++ JapaneseLookup/ConfigManager.cs
@@ -9,12 +9,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Markup;
 
 namespace JapaneseLookup
 {
     internal static class ConfigManager
     {
         public static readonly string ApplicationPath = Directory.GetCurrentDirectory();
+        private static readonly List<string> japaneseFonts = FindJapaneseFonts().OrderBy(font => font).ToList();
         public static int MaxSearchLength = int.Parse(ConfigurationManager.AppSettings.Get("MaxSearchLength"));
         public static string FrequencyList = ConfigurationManager.AppSettings.Get("FrequencyList");
         public static string AnkiConnectUri = ConfigurationManager.AppSettings.Get("AnkiConnectUri");
@@ -36,6 +38,7 @@ namespace JapaneseLookup
             MaxSearchLength = int.Parse(ConfigurationManager.AppSettings.Get("MaxSearchLength"));
             mainWindow.OpacitySlider.Value = double.Parse(ConfigurationManager.AppSettings.Get("MainWindowOpacity"));
             mainWindow.FontSizeSlider.Value = double.Parse(ConfigurationManager.AppSettings.Get("MainWindowFontSize"));
+            mainWindow.MainTextBox.FontFamily = new FontFamily(ConfigurationManager.AppSettings.Get("MainWindowFont"));
             mainWindow.MainTextBox.FontSize = mainWindow.FontSizeSlider.Value;
             mainWindow.Background =
                 (SolidColorBrush) new BrushConverter().ConvertFrom(
@@ -48,7 +51,7 @@ namespace JapaneseLookup
             mainWindow.Width = double.Parse(ConfigurationManager.AppSettings.Get("MainWindowWidth"));
             mainWindow.Top = double.Parse(ConfigurationManager.AppSettings.Get("MainWindowTopPosition"));
             mainWindow.Left = double.Parse(ConfigurationManager.AppSettings.Get("MainWindowLeftPosition"));
-
+            
             ConfigurationManager.RefreshSection("appSettings");
         }
 
@@ -65,6 +68,8 @@ namespace JapaneseLookup
                 preferenceWindow.TextboxTextSizeNumericUpDown.Value.ToString();
             config.AppSettings.Settings["MainWindowOpacity"].Value =
                 preferenceWindow.TextboxOpacityNumericUpDown.Value.ToString();
+            config.AppSettings.Settings["MainWindowFont"].Value =
+                preferenceWindow.FontComboBox.SelectedItem.ToString();
             var mainWindow = Application.Current.Windows.OfType<MainWindow>().First();
             config.AppSettings.Settings["MainWindowHeight"].Value = mainWindow.Height.ToString();
             config.AppSettings.Settings["MainWindowWidth"].Value = mainWindow.Width.ToString();
@@ -86,6 +91,35 @@ namespace JapaneseLookup
             preferenceWindow.TextboxTextColorButton.Background = mainWindow.MainTextBox.Foreground;
             preferenceWindow.TextboxTextSizeNumericUpDown.Value = (decimal) mainWindow.FontSizeSlider.Value;
             preferenceWindow.TextboxOpacityNumericUpDown.Value = (decimal) mainWindow.OpacitySlider.Value;
+            preferenceWindow.FontComboBox.ItemsSource = japaneseFonts;
+            preferenceWindow.FontComboBox.SelectedItem = mainWindow.MainTextBox.FontFamily.ToString();
+        }
+
+
+        public static List<string> FindJapaneseFonts()
+        {
+            List<string> japaneseFonts = new List<string>();
+            foreach (FontFamily fontFamily in Fonts.SystemFontFamilies)
+            {
+                if (fontFamily.FamilyNames.Keys.Contains(XmlLanguage.GetLanguage("ja-jp")))
+                    japaneseFonts.Add(fontFamily.Source);
+
+                else if (fontFamily.FamilyNames.Keys.Count == 1 && fontFamily.FamilyNames.Keys.Contains(XmlLanguage.GetLanguage("en-US")))
+                {
+                    foreach (var typeFace in fontFamily.GetTypefaces())
+                    {
+                        if (typeFace.TryGetGlyphTypeface(out var glyphTypeFace))
+                        {
+                            if (glyphTypeFace.CharacterToGlyphMap.ContainsKey(20685))
+                            {
+                                japaneseFonts.Add(fontFamily.Source);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return japaneseFonts;
         }
     }
 }
