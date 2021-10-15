3fc8eb0795476e5f44482ec9d47d48c3c9ff05b2
rampaa <rampaa@waifuland.com>
2021-10-15 06:47:36 +0300

    :tehepero:

--- JapaneseLookup/EDICT/JMdict/JMdictEntry.cs
+++ JapaneseLookup/EDICT/JMdict/JMdictEntry.cs
@@ -8,13 +8,11 @@ namespace JapaneseLookup.EDICT.JMdict
         public List<KEle> KEleList { get; set; }
         public List<REle> REleList { get; set; }
         public List<Sense> SenseList { get; set; }
-        public List<Trans> TransList { get; set; }
         public JMdictEntry()
         {
             KEleList = new List<KEle>();
             REleList = new List<REle>();
             SenseList = new List<Sense>();
-            TransList = new List<Trans>();
         }
     }
 }
--- JapaneseLookup/EDICT/JMnedict/Trans.cs
+++ JapaneseLookup/EDICT/JMnedict/Trans.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace JapaneseLookup.EDICT
+namespace JapaneseLookup.EDICT.JMnedict
 {
     public class Trans
     {
