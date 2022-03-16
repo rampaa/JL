38617c62e77e9b7294412660790968c223b6a838
rampaa <rampaa@waifuland.com>
2022-03-16 21:34:33 +0300

    Cave in

--- JL.Core/Kana.cs
+++ JL.Core/Kana.cs
@@ -180,7 +180,7 @@ namespace JL.Core
         public static string KatakanaToHiraganaConverter(string text)
         {
             StringBuilder textInHiragana = new();
-            List<string> unicodeCharacters = text.Normalize(NormalizationForm.FormKC).UnicodeIterator().ToList();
+            List<string> unicodeCharacters = text.Normalize(NormalizationForm.FormKD).UnicodeIterator().ToList();
             int listLength = unicodeCharacters.Count;
             for (int i = 0; i < listLength; i++)
             {
