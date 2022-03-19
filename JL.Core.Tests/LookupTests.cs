bf260f41ba38d2ed1c22450a72dbfeccfcec0556
mertvn <mertvn@protonmail.com>
2022-03-19 21:38:02 +0300

    Add freq tests

--- JL.Core.Tests/LookupTests.cs
+++ JL.Core.Tests/LookupTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using JL.Core.Dicts;
 using JL.Core.Dicts.EDICT;
@@ -31,6 +32,7 @@ namespace JL.Core.Tests
             }
 
             JMdictLoader.Load(Storage.Dicts[DictType.JMdict].Path).Wait();
+            Storage.LoadFrequency().Wait();
         }
 
         [Test]
@@ -38,7 +40,7 @@ namespace JL.Core.Tests
         {
             // Arrange
             string expected =
-                "[{\"foundSpelling\":[\"始まる\"],\"readings\":[\"はじまる\"],\"definitions\":[\"(v5r, vi) (1) to begin; to start; to commence (v5r, vi) (2) to happen (again); to begin (anew) (v5r, vi) (3) to date (from); to originate (in) \"],\"foundForm\":[\"始まる\"],\"EdictID\":[\"1307500\"],\"alternativeSpellings\":[],\"process\":[],\"frequency\":[\"2147483647\"],\"pOrthographyInfoList\":[],\"rOrthographyInfoList\":[\"\"],\"aOrthographyInfoList\":[],\"DictType\":[\"JMdict\"]}]";
+                "[{\"foundSpelling\":[\"始まる\"],\"readings\":[\"はじまる\"],\"definitions\":[\"(v5r, vi) (1) to begin; to start; to commence (v5r, vi) (2) to happen (again); to begin (anew) (v5r, vi) (3) to date (from); to originate (in) \"],\"foundForm\":[\"始まる\"],\"EdictID\":[\"1307500\"],\"alternativeSpellings\":[],\"process\":[],\"frequency\":[\"759\"],\"pOrthographyInfoList\":[],\"rOrthographyInfoList\":[\"\"],\"aOrthographyInfoList\":[],\"DictType\":[\"JMdict\"]}]";
 
             string text = "始まる";
 
@@ -46,6 +48,216 @@ namespace JL.Core.Tests
             List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
                 text);
             string actual = JsonSerializer.Serialize(result, Storage.JsoUnsafeEscaping);
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_た_他()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "294";
+
+            string text = "た";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.FoundSpelling].Contains("他"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_た_多()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "9844";
+
+            string text = "た";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.FoundSpelling].Contains("多"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_た_田()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "21431";
+
+            string text = "た";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.FoundSpelling].Contains("田"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_日_ひ()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "227";
+
+            string text = "日";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.Readings].Contains("ひ"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_日_にち()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "777";
+
+            string text = "日";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.Readings].Contains("にち"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_日_か()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "1105";
+
+            string text = "日";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.Readings].Contains("か"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_あんまり_余り()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "284";
+
+            string text = "余り";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.FoundSpelling].Contains("余り"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_懐かしい_いだく()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "903";
+
+            string text = "懐かしい";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.Readings].Contains("いだく"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_廃虚_はいきょ()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "8560";
+
+            string text = "廃虚";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.Readings].Contains("はいきょ"))[LookupResult.Frequency][0];
+
+            // Assert
+            StringAssert.AreEqualIgnoringCase(expected, actual);
+        }
+
+        [Test]
+        public void Freq_廃墟_はいきょ()
+        {
+            Storage.LoadFrequency().Wait();
+
+            // Arrange
+            string expected = "8560";
+
+            string text = "廃墟";
+
+            // Act
+            List<Dictionary<LookupResult, List<string>>> result = Lookup.Lookup.LookupText(
+                text);
+            string actual =
+                result.First(x =>
+                    x[LookupResult.Readings].Contains("はいきょ"))[LookupResult.Frequency][0];
 
             // Assert
             StringAssert.AreEqualIgnoringCase(expected, actual);
