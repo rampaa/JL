e69dd7a2bbad33e35ca5451081a4b150cdd422b1
rampaa <rampaa@waifuland.com>
2022-03-27 16:01:09 +0300

    Add deconjugation support for v5aru and vz.

--- JL.Core/Deconjugation/Form.cs
+++ JL.Core/Deconjugation/Form.cs
@@ -16,5 +16,54 @@
             Seentext = seentext;
             Process = process;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            Form form = obj as Form;
+
+            return Text == form.Text
+                   && OriginalText == form.OriginalText
+                   && Tags.SequenceEqual(form.Tags)
+                   && Seentext.SetEquals(form.Seentext)
+                   && Process.SequenceEqual(form.Process);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+
+                hash = hash * 37 + Text?.GetHashCode() ?? 0;
+                hash = hash * 37 + OriginalText?.GetHashCode() ?? 0;
+
+                if (Tags == null)
+                    hash *= 37;
+
+                else
+                    foreach (string tag in Tags)
+                        hash = hash * 37 + tag?.GetHashCode() ?? 0;
+
+                if (Process == null)
+                    hash *= 37;
+
+                else
+                    foreach (string process in Process)
+                        hash = hash * 37 + process?.GetHashCode() ?? 0;
+
+                if (Seentext == null)
+                    hash *= 37;
+
+                else
+                    foreach (string seenText in Seentext)
+                        hash = hash * 37 + seenText?.GetHashCode() ?? 0;
+
+                return hash;
+            }
+        }
+
     }
 }
