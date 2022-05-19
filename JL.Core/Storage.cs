c9d0dedbe4e3c7d3f31f0390558a49366ef2302a
rampaa <rampaa@waifuland.com>
2022-05-19 17:25:28 +0300

    A new month a new release

--- JL.Core/Storage.cs
+++ JL.Core/Storage.cs
@@ -28,7 +28,7 @@ public static class Storage
     public static readonly string ResourcesPath = Path.Join(AppContext.BaseDirectory, "Resources");
     public static readonly string ConfigPath = Path.Join(AppContext.BaseDirectory, "Config");
     public static readonly HttpClient Client = new(new HttpClientHandler { UseProxy = false });
-    public static readonly Version Version = new(1, 9);
+    public static readonly Version Version = new(1, 10, 0);
     public static readonly string RepoUrl = "https://github.com/rampaa/JL/";
     public static readonly Uri JmdictUrl = new("http://ftp.edrdg.org/pub/Nihongo/JMdict_e.gz");
     public static readonly Uri JmnedictUrl = new("http://ftp.edrdg.org/pub/Nihongo/JMnedict.xml.gz");
