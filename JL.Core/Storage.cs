6f8bf9f48f93b0c49bc93fde3283651d1c827456
rampaa <rampaa@waifuland.com>
2022-09-18 19:44:34 +0300

    Update dependency and bump version number

--- JL.Core/Storage.cs
+++ JL.Core/Storage.cs
@@ -33,7 +33,7 @@ public static class Storage
     public static readonly string ResourcesPath = Path.Join(AppContext.BaseDirectory, "Resources");
     public static readonly string ConfigPath = Path.Join(AppContext.BaseDirectory, "Config");
     public static readonly HttpClient Client = new(new HttpClientHandler { UseProxy = false }) { Timeout = TimeSpan.FromMinutes(10) };
-    public static readonly Version JLVersion = new(1, 13, 0);
+    public static readonly Version JLVersion = new(1, 14, 0);
     public static readonly Uri GitHubApiUrlForLatestJLRelease = new("https://api.github.com/repos/rampaa/JL/releases/latest");
     public static readonly Uri JmdictUrl = new("https://www.edrdg.org/pub/Nihongo/JMdict_e.gz");
     public static readonly Uri JmnedictUrl = new("https://www.edrdg.org/pub/Nihongo/JMnedict.xml.gz");
