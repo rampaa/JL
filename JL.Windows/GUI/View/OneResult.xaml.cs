4800a4e855c09d6856071c5dd7cc56251b6943b6
mertvn <mertvn@protonmail.com>
2022-09-21 12:40:34 +0300

    Adapt pitch accent generation for OneResult

--- JL.Windows/GUI/View/OneResult.xaml.cs
+++ JL.Windows/GUI/View/OneResult.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
 using JL.Core;
 using JL.Core.Dicts;
 using JL.Core.Lookup;
+using JL.Core.Pitch;
 using JL.Windows.GUI.ViewModel;
 using JL.Windows.Utilities;
 
@@ -15,10 +18,10 @@ public partial class OneResult : UserControl
 
     private PopupWindow PopupWindow { get; }
 
-    public OneResult(LookupResult lr, PopupWindow popupWindow, int index)
+    public OneResult(LookupResult result, PopupWindow popupWindow, int index)
     {
         InitializeComponent();
-        Vm = new OneResultViewModel(lr, popupWindow.Vm, index);
+        Vm = new OneResultViewModel(result, popupWindow.Vm, index);
         DataContext = Vm;
         PopupWindow = popupWindow;
 
@@ -29,6 +32,153 @@ public partial class OneResult : UserControl
         //         FontSize = 17, Text = vm.Definitions,TextWrapping = TextWrapping.Wrap,IsReadOnly = true,IsUndoEnabled = false
         //     });
         // }
+
+        Dict? pitchDict = Storage.Dicts.Values.FirstOrDefault(dict => dict.Type == DictType.PitchAccentYomichan);
+        if (pitchDict?.Active ?? false)
+        {
+            if (string.IsNullOrEmpty(Vm.Readings))
+            {
+                List<string>? readings = result.Readings;
+                var textBlock = TextBlockPrimarySpelling;
+
+                List<Polyline> pitchAccentGrid = CreatePitchAccentGrid(result.PrimarySpelling,
+                    result.AlternativeSpellings ?? new(),
+                    readings ?? new(),
+                    Vm.PrimarySpelling.Split(", ").ToList(),
+                    textBlock.Margin.Left,
+                    pitchDict);
+
+                if (pitchAccentGrid.Any())
+                {
+                    foreach (Polyline polyline in pitchAccentGrid)
+                    {
+                        pitchAccentGridPrimarySpelling.Children.Add(polyline);
+                    }
+                }
+            }
+            else
+            {
+                List<string>? readings = result.Readings;
+                var textBlock = TextBoxReadings;
+
+                List<Polyline> pitchAccentGrid = CreatePitchAccentGrid(result.PrimarySpelling,
+                    result.AlternativeSpellings ?? new(),
+                    readings ?? new(),
+                    Vm.Readings.Split(", ").ToList(),
+                    textBlock.Margin.Left,
+                    pitchDict);
+
+                if (pitchAccentGrid.Any())
+                {
+                    foreach (Polyline polyline in pitchAccentGrid)
+                    {
+                        pitchAccentGridReadings.Children.Add(polyline);
+                    }
+                }
+            }
+        }
+    }
+
+    private static List<Polyline> CreatePitchAccentGrid(string primarySpelling, List<string> alternativeSpellings,
+        List<string> readings, List<string> splitReadingsWithRInfo, double leftMargin, Dict dict)
+    {
+        List<Polyline> pitchAccentGrid = new();
+
+        bool hasReading = readings.Any();
+
+        int fontSize = hasReading
+            ? ConfigManager.ReadingsFontSize
+            : ConfigManager.PrimarySpellingFontSize;
+
+        List<string> expressions = hasReading ? readings : new List<string> { primarySpelling };
+
+        double horizontalOffsetForReading = leftMargin;
+
+        for (int i = 0; i < expressions.Count; i++)
+        {
+            string normalizedExpression = Kana.KatakanaToHiraganaConverter(expressions[i]);
+            List<string> combinedFormList = Kana.CreateCombinedForm(expressions[i]);
+
+            if (i > 0)
+            {
+                horizontalOffsetForReading +=
+                    WindowsUtils.MeasureTextSize(splitReadingsWithRInfo[i - 1] + ", ", fontSize).Width;
+            }
+
+            if (dict.Contents.TryGetValue(normalizedExpression, out List<IResult>? pitchAccentDictResultList))
+            {
+                PitchResult? chosenPitchAccentDictResult = null;
+
+                for (int j = 0; j < pitchAccentDictResultList.Count; j++)
+                {
+                    var pitchAccentDictResult = (PitchResult)pitchAccentDictResultList[j];
+
+                    if (!hasReading || (pitchAccentDictResult.Reading != null &&
+                                        normalizedExpression ==
+                                        Kana.KatakanaToHiraganaConverter(pitchAccentDictResult.Reading)))
+                    {
+                        if (primarySpelling == pitchAccentDictResult.Spelling)
+                        {
+                            chosenPitchAccentDictResult = pitchAccentDictResult;
+                            break;
+                        }
+                        else if (alternativeSpellings?.Contains(pitchAccentDictResult.Spelling) ?? false)
+                        {
+                            chosenPitchAccentDictResult ??= pitchAccentDictResult;
+                        }
+                    }
+                }
+
+                if (chosenPitchAccentDictResult != null)
+                {
+                    Polyline polyline = new()
+                    {
+                        StrokeThickness = 2,
+                        Stroke = (SolidColorBrush)new BrushConverter()
+                            .ConvertFrom(dict.Options?.PitchAccentMarkerColor?.Value
+                                         ?? Colors.DeepSkyBlue.ToString())!,
+                        StrokeDashArray = new DoubleCollection { 1, 1 }
+                    };
+
+                    bool lowPitch = false;
+                    double horizontalOffsetForChar = horizontalOffsetForReading;
+                    for (int j = 0; j < combinedFormList.Count; j++)
+                    {
+                        Size charSize = WindowsUtils.MeasureTextSize(combinedFormList[j], fontSize);
+
+                        if (chosenPitchAccentDictResult.Position - 1 == j)
+                        {
+                            polyline.Points!.Add(new Point(horizontalOffsetForChar, 0));
+                            polyline.Points.Add(new Point(horizontalOffsetForChar + charSize.Width, 0));
+                            polyline.Points.Add(new Point(horizontalOffsetForChar + charSize.Width,
+                                charSize.Height));
+
+                            lowPitch = true;
+                        }
+                        else if (j == 0)
+                        {
+                            polyline.Points!.Add(new Point(horizontalOffsetForChar, charSize.Height));
+                            polyline.Points.Add(new Point(horizontalOffsetForChar + charSize.Width,
+                                charSize.Height));
+                            polyline.Points.Add(new Point(horizontalOffsetForChar + charSize.Width, 0));
+                        }
+                        else
+                        {
+                            double charHeight = lowPitch ? charSize.Height : 0;
+                            polyline.Points!.Add(new Point(horizontalOffsetForChar, charHeight));
+                            polyline.Points.Add(new Point(horizontalOffsetForChar + charSize.Width,
+                                charHeight));
+                        }
+
+                        horizontalOffsetForChar += charSize.Width;
+                    }
+
+                    pitchAccentGrid.Add(polyline);
+                }
+            }
+        }
+
+        return pitchAccentGrid;
     }
 
     private void Unselect(object sender, RoutedEventArgs e)
