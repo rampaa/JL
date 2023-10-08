c6103b066db92fde811cb61dbbd22389173eaeb5
rampaa <rampaa@waifuland.com>
2023-10-08 19:14:14 +0300

    Minor

--- JL.Core.Tests/LookupTests.cs
+++ JL.Core.Tests/LookupTests.cs
@@ -64,9 +64,7 @@ public class LookupTests
                     deconjugatedMatchedText: "始まる",
                     readings: new[] { "はじまる" },
                     formattedDefinitions: "(v5r, vi) (1) to begin; to start; to commence (v5r, vi) (2) to happen (again); to begin (anew) (v5r, vi) (3) to date (from); to originate (in)",
-                    edictId: 1307500,
-                    alternativeSpellingsOrthographyInfoList: null,
-                    readingsOrthographyInfoList: null
+                    edictId: 1307500
                 )
             };
 
