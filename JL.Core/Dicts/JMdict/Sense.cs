18e11433eed52ce64590370281b93f7367bd4851
rampaa <rampaa@waifuland.com>
2024-10-17 20:10:12 +0300

    Minor

--- JL.Core/Dicts/JMdict/Sense.cs
+++ JL.Core/Dicts/JMdict/Sense.cs
@@ -18,7 +18,7 @@ internal sealed class Sense(
     public List<string> PosList { get; } = posList; // e.g. "noun"
     public List<string> FieldList { get; } = fieldList; // e.g. "martial arts"
     public List<string> MiscList { get; } = miscList; // e.g. "abbr"
-    public string? SInf { get; set; } = sInf; // e.g. "often derog"
+    public string? SInf { get; } = sInf; // e.g. "often derog"
     public List<string> DialList { get; } = dialList; // e.g. ksb
     public List<string> GlossList { get; } = glossList; // English meaning
     public List<string> XRefList { get; } = xRefList; // Related terms
