fd8c3664e23f61690e91bb99d9972335db6efa0a
rampaa <rampaa@waifuland.com>
2024-11-18 18:25:22 +0300

    Minor

--- JL.Windows/update-helper.cmd
+++ JL.Windows/update-helper.cmd
@@ -2,6 +2,7 @@
 @SETLOCAL enableextensions
 @CD /D "%~dp0"
 TASKKILL /F /T /PID %1
+DEL /Q /F ".\tmp\PoS.json"
 ROBOCOPY ".\tmp\ " . /E /Z /R:30 /W:1 /MOVE
 RMDIR /Q /S  "%CD%\tmp\"
 START "" "%CD%\JL.exe"
