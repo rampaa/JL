3261dbda86861619931cc1663317ad2f057df6af
rampaa <rampaa@waifuland.com>
2024-12-22 20:16:10 +0300

    Minor

--- JL.Core/Dicts/JMdict/LoanwordSource.cs
+++ JL.Core/Dicts/JMdict/LoanwordSource.cs
@@ -1,3 +1,3 @@
 namespace JL.Core.Dicts.JMdict;
 
-internal readonly record struct LoanwordSource(string Language, bool IsPart, bool IsWasei, string? OriginalWord);
+internal readonly record struct LoanwordSource(string Language, bool IsPart, bool IsWasei, string? OriginalWord = null);
