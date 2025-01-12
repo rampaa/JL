703e18ab9f0d3edb5e1924326e97f0efe9962c38
rampaa <rampaa@waifuland.com>
2025-01-12 12:44:54 +0300

    Bump the version number

--- JL.Core/Utilities/Utils.cs
+++ JL.Core/Utilities/Utils.cs
@@ -23,7 +23,7 @@ namespace JL.Core.Utilities;
 
 public static partial class Utils
 {
-    public static readonly Version JLVersion = new(3, 0, 0);
+    public static readonly Version JLVersion = new(3, 1, 0);
     public static readonly string ApplicationPath = AppContext.BaseDirectory;
     public static readonly string ResourcesPath = Path.Join(ApplicationPath, "Resources");
     public static readonly string ConfigPath = Path.Join(ApplicationPath, "Config");
