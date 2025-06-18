266b6d58c365e333ccd2650362edbc99b65204d6
rampaa <rampaa@waifuland.com>
2025-06-18 03:18:50 +0300

    Minor

--- JL.Core/Network/MpvUtils.cs
+++ JL.Core/Network/MpvUtils.cs
@@ -7,6 +7,10 @@ using JL.Core.Utilities;
 namespace JL.Core.Network;
 public static class MpvUtils
 {
+    private static readonly ReadOnlyMemory<byte> s_getPausePropertyCommand = NetworkUtils.s_utf8NoBom.GetBytes(/*lang=json,strict*/ "{\"command\":[\"get_property\",\"pause\"]}\n");
+    private static readonly ReadOnlyMemory<byte> s_pauseCommand = NetworkUtils.s_utf8NoBom.GetBytes(/*lang=json,strict*/ "{\"command\":[\"set_property\",\"pause\",true]}\n");
+    private static readonly ReadOnlyMemory<byte> s_unpauseCommand = NetworkUtils.s_utf8NoBom.GetBytes(/*lang=json,strict*/ "{\"command\":[\"set_property\",\"pause\",false]}\n");
+
     private static long s_lastPausedByJLTimestamp;
     private static bool s_pausedByJL; // = false
 
@@ -14,22 +18,24 @@ public static class MpvUtils
     {
         try
         {
-            NamedPipeClientStream pipeClient = new(".", CoreConfigManager.Instance.MpvNamedPipePath, PipeDirection.InOut);
+            NamedPipeClientStream pipeClient = new(".", CoreConfigManager.Instance.MpvNamedPipePath, PipeDirection.InOut, PipeOptions.Asynchronous);
             await using (pipeClient.ConfigureAwait(false))
             {
                 await pipeClient.ConnectAsync(1500).ConfigureAwait(false);
+                await pipeClient.WriteAsync(s_getPausePropertyCommand).ConfigureAwait(false);
 
-                byte[] getPausePropertyCommand = NetworkUtils.s_utf8NoBom.GetBytes(/*lang=json,strict*/ "{\"command\":[\"get_property\",\"pause\"]}\n");
-                await pipeClient.WriteAsync(getPausePropertyCommand).ConfigureAwait(false);
+                using StreamReader reader = new(pipeClient);
+                string? responseJson = await reader.ReadLineAsync().ConfigureAwait(false);
+                if (responseJson is null)
+                {
+                    return;
+                }
 
-                byte[] buffer = new byte[128];
-                int bytesRead = await pipeClient.ReadAsync(buffer).ConfigureAwait(false);
-                string response = NetworkUtils.s_utf8NoBom.GetString(buffer, 0, bytesRead);
-                bool isPaused = JsonDocument.Parse(response).RootElement.GetProperty("data").GetBoolean();
+                using JsonDocument responJsonDocument = JsonDocument.Parse(responseJson);
+                bool isPaused = responJsonDocument.RootElement.GetProperty("data").GetBoolean();
                 if (!isPaused)
                 {
-                    byte[] pauseCommand = NetworkUtils.s_utf8NoBom.GetBytes(/*lang=json,strict*/ "{\"command\":[\"set_property\",\"pause\",true]}\n");
-                    await pipeClient.WriteAsync(pauseCommand).ConfigureAwait(false);
+                    await pipeClient.WriteAsync(s_pauseCommand).ConfigureAwait(false);
 
                     s_pausedByJL = true;
                     s_lastPausedByJLTimestamp = Stopwatch.GetTimestamp();
@@ -50,12 +56,12 @@ public static class MpvUtils
         catch (TimeoutException)
         {
             s_pausedByJL = false;
-            Utils.Logger.Warning($"Connection timed out. Is MPV currently running with its IPC server properly configured? Make sure to add input-ipc-server={CoreConfigManager.Instance.MpvNamedPipePath} to your mpv.conf file.");
+            Utils.Logger.Warning("Connection timed out. Is mpv currently running with its IPC server properly configured? Make sure to add input-ipc-server={MpvNamedPipePath} to your mpv.conf file.", CoreConfigManager.Instance.MpvNamedPipePath);
         }
         catch (Exception ex)
         {
             s_pausedByJL = false;
-            Utils.Logger.Error(ex, "An unexpecteed error occurred while attempting to puase playback in MPV");
+            Utils.Logger.Error(ex, "An unexpected error occurred while attempting to pause playback in mpv");
         }
     }
 
@@ -68,23 +74,22 @@ public static class MpvUtils
 
         try
         {
-            NamedPipeClientStream pipeClient = new(".", CoreConfigManager.Instance.MpvNamedPipePath, PipeDirection.Out);
+            NamedPipeClientStream pipeClient = new(".", CoreConfigManager.Instance.MpvNamedPipePath, PipeDirection.Out, PipeOptions.Asynchronous);
             await using (pipeClient.ConfigureAwait(false))
             {
                 await pipeClient.ConnectAsync(1500).ConfigureAwait(false);
-                byte[] pauseCommand = NetworkUtils.s_utf8NoBom.GetBytes(/*lang=json,strict*/ "{\"command\":[\"set_property\",\"pause\",false]}\n");
-                await pipeClient.WriteAsync(pauseCommand).ConfigureAwait(false);
+                await pipeClient.WriteAsync(s_unpauseCommand).ConfigureAwait(false);
             }
         }
         catch (TimeoutException)
         {
             s_pausedByJL = false;
-            Utils.Logger.Warning("Connection timed out. Is MPV running?");
+            Utils.Logger.Warning("Connection timed out. Is mpv running?");
         }
         catch (Exception ex)
         {
             s_pausedByJL = false;
-            Utils.Logger.Error(ex, "An unexpecteed error occurred while attempting to resume playback in MPV");
+            Utils.Logger.Error(ex, "An unexpected error occurred while attempting to resume playback in mpv");
         }
     }
 }
