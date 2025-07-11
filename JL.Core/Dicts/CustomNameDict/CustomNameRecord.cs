1888f3fac444a40819d019e97459c899ffd4f881
rampaa <rampaa@waifuland.com>
2025-07-11 07:19:14 +0300

    Minor

--- JL.Core/Dicts/CustomNameDict/CustomNameRecord.cs
+++ JL.Core/Dicts/CustomNameDict/CustomNameRecord.cs
@@ -2,7 +2,7 @@ using JL.Core.Dicts.Interfaces;
 
 namespace JL.Core.Dicts.CustomNameDict;
 
-internal sealed record class CustomNameRecord : IDictRecordWithSingleReading
+internal sealed class CustomNameRecord : IDictRecordWithSingleReading, IEquatable<CustomNameRecord>
 {
     public string PrimarySpelling { get; }
     public string? Reading { get; }
@@ -21,4 +21,33 @@ internal sealed record class CustomNameRecord : IDictRecordWithSingleReading
     {
         return $"[{NameType}] {Reading ?? PrimarySpelling}{(ExtraInfo is not null ? $"\n{ExtraInfo}" : "")}";
     }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is CustomNameRecord customNameRecord
+               && PrimarySpelling == customNameRecord.PrimarySpelling
+               && Reading == customNameRecord.Reading
+               && NameType == customNameRecord.NameType
+               && ExtraInfo == customNameRecord.ExtraInfo;
+    }
+
+    public bool Equals(CustomNameRecord? other)
+    {
+        return other is not null
+               && PrimarySpelling == other.PrimarySpelling
+               && Reading == other.Reading
+               && NameType == other.NameType
+               && ExtraInfo == other.ExtraInfo;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(PrimarySpelling.GetHashCode(StringComparison.Ordinal),
+            Reading?.GetHashCode(StringComparison.Ordinal) ?? 37,
+            NameType.GetHashCode(StringComparison.Ordinal),
+            ExtraInfo?.GetHashCode(StringComparison.Ordinal) ?? 37);
+    }
+
+    public static bool operator ==(CustomNameRecord? left, CustomNameRecord? right) => left?.Equals(right) ?? right is null;
+    public static bool operator !=(CustomNameRecord? left, CustomNameRecord? right) => !left?.Equals(right) ?? right is not null;
 }
