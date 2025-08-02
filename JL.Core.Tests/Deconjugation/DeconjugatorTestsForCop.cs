9bc343a7487d92c04acd3b0793c6d0015914ad54
rampaa <rampaa@waifuland.com>
2025-08-02 23:45:04 +0300

    Add more tests

---
+++ JL.Core.Tests/Deconjugation/DeconjugatorTestsForCop.cs
@@ -0,0 +1,54 @@
+using JL.Core.Deconjugation;
+using JL.Core.Lookup;
+using JL.Core.Utilities;
+using NUnit.Framework;
+
+namespace JL.Core.Tests.Deconjugation;
+
+[TestFixture]
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes
+internal sealed class DeconjugatorTestsForCop
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
+{
+    [OneTimeSetUp]
+    public void ClassInit()
+    {
+        DeconjugatorUtils.DeserializeRules().Wait();
+    }
+
+    [Test]
+    public void Deconjugate_Past_Cop()
+    {
+        const string termToDeconjugate = "でした";
+        const string expected = "～past";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "です" && form.Tags[^1] is "cop").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_Conditional_Cop()
+    {
+        const string termToDeconjugate = "でしたら";
+        const string expected = "～conditional";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "です" && form.Tags[^1] is "cop").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_Conjectural_Cop()
+    {
+        const string termToDeconjugate = "でしょう";
+        const string expected = "～conjectural";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "です" && form.Tags[^1] is "cop").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_Te_Cop()
+    {
+        const string termToDeconjugate = "でして";
+        const string expected = "～te";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "です" && form.Tags[^1] is "cop").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
