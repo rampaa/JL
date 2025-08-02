0254c57e470c48935284782ffae1305fcebebc35
rampaa <rampaa@waifuland.com>
2025-08-02 14:23:20 +0300

    Minor

--- JL.Windows/LookupDisplayResult.cs
+++ JL.Windows/LookupDisplayResult.cs
@@ -4,9 +4,9 @@ using JL.Windows.GUI;
 namespace JL.Windows;
 internal sealed class LookupDisplayResult(PopupWindow popupWindow, LookupResult lookupResult, int index, bool nonLastItem)
 {
-    public LookupResult LookupResult { get; } = lookupResult;
     public PopupWindow OwnerWindow { get; } = popupWindow;
+    public LookupResult LookupResult { get; } = lookupResult;
     public int Index { get; } = index;
     public bool NonLastItem { get; } = nonLastItem;
-    public bool IsDuplicate { get; set; }
+    public bool IsDuplicate { get; set; } // = false;
 }
