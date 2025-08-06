fa2cb3dca5173f93ed97c72da27a2cb297b93609
rampaa <rampaa@waifuland.com>
2025-08-06 23:20:12 +0300

    Add tests for conjugations of i adjectives

---
+++ JL.Core.Tests/Deconjugation/DeconjugatorTestsForAdjI.cs
@@ -0,0 +1,207 @@
+using JL.Core.Deconjugation;
+using JL.Core.Lookup;
+using JL.Core.Utilities;
+using NUnit.Framework;
+
+namespace JL.Core.Tests.Deconjugation;
+
+[TestFixture]
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes
+internal sealed class DeconjugatorTestsForAdjI
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
+{
+    [OneTimeSetUp]
+    public void ClassInit()
+    {
+        DeconjugatorUtils.DeserializeRules().Wait();
+    }
+
+    [Test]
+    public void Deconjugate_Stem_AdjI()
+    {
+        const string termToDeconjugate = "小さ";
+        const string expected = "～stem";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainNonPastNegative_AdjI()
+    {
+        const string termToDeconjugate = "小さくない";
+        const string expected = "～negative";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainPastAffirmative_AdjI()
+    {
+        const string termToDeconjugate = "小さかった";
+        const string expected = "～past";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainPastNegative_AdjI()
+    {
+        const string termToDeconjugate = "小さくなかった";
+        const string expected = "～negative→past";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainTeFormAffirmative_AdjI()
+    {
+        const string termToDeconjugate = "小さくて";
+        const string expected = "～te";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainTeFormNegative_AdjI()
+    {
+        const string termToDeconjugate = "小さくなくて";
+        const string expected = "～negative→te";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_ProvisionalConditionalAffirmative_AdjI()
+    {
+        const string termToDeconjugate = "小さければ";
+        const string expected = "～provisional conditional";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_ProvisionalConditionalNegative_AdjI()
+    {
+        const string termToDeconjugate = "小さくなければ";
+        const string expected = "～negative→provisional conditional";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_TemporalConditionalAffirmative_AdjI()
+    {
+        const string termToDeconjugate = "小さかったら";
+        const string expected = "～conditional";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_FormalConditionalAffirmative_AdjI()
+    {
+        const string termToDeconjugate = "小さかったらば";
+        const string expected = "～formal conditional";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_TemporalConditionalNegative_AdjI()
+    {
+        const string termToDeconjugate = "小さくなかったら";
+        const string expected = "～negative→conditional";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainNonPastSugiruAffirmative_AdjI()
+    {
+        const string termToDeconjugate = "小さすぎる";
+        const string expected = "～excess";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainNonPastSouAffirmative_AdjI()
+    {
+        const string termToDeconjugate = "小さそう";
+        const string expected = "～seemingness";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_TopicOrCondition_AdjI()
+    {
+        const string termToDeconjugate = "小さくては";
+        const string expected = "～topic/condition";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_ContractedTopicOrConditionCha_AdjI()
+    {
+        const string termToDeconjugate = "小さくちゃ";
+        const string expected = "～topic/condition→contracted";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainNonPastContractedProvisionalConditionalNegativeKya_AdjI()
+    {
+        const string termToDeconjugate = "小さくなきゃ";
+        const string expected = "～negative→provisional conditional→contracted";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainNonPastKansaibenNegative_AdjI()
+    {
+        const string termToDeconjugate = "小さくへん";
+        const string expected = "～negative→ksb";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_PlainPastKansaibenNegative_AdjI()
+    {
+        const string termToDeconjugate = "小さくへんかった";
+        const string expected = "～negative→ksb→past";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_ContractedProvisionalConditionalRya_AdjI()
+    {
+        const string termToDeconjugate = "小さきゃ";
+        const string expected = "～provisional conditional→contracted";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_AdverbialStem_AdjI()
+    {
+        const string termToDeconjugate = "小さく";
+        const string expected = "～adverbial stem";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Deconjugate_NounForm_AdjI()
+    {
+        const string termToDeconjugate = "小ささ";
+        const string expected = "～noun form";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
