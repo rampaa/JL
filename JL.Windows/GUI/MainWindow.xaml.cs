rampaa/JL

commit 473b352672bf75077e558c81644f7c01069b7cde

Prevent popup from appearing in the task switcher on Windows 11

JL.Windows/GUI/MainWindow.xaml.cs
@@ -143,6 +143,8 @@ internal sealed partial class MainWindow : IDisposable
         FirstPopupWindow.Show();
         FirstPopupWindow.HidePopup();
 
+        FirstPopupWindow.Owner = this;
+
         FocusManager.SetFocusedElement(this, MainTextBox);
         // Makes caret/highlight visible without any mouse click
         EditingCommands.MoveLeftByCharacter.Execute(null, MainTextBox);

commit 34556cca38875f8dc1f0266f62fd7c1acd4cba86

Fix Tsukikage lookups not working when JL is minimized (https://github.com/rampaa/Tsukikage/issues/1#issuecomment-3863531556)
The regression was introduced in https://github.com/rampaa/JL/commit/473b352672bf75077e558c81644f7c01069b7cde

JL.Windows/GUI/MainWindow.xaml.cs
@@ -2125,6 +2125,10 @@ internal sealed partial class MainWindow : IDisposable
         CoreConfigManager coreConfigManager = CoreConfigManager.Instance;
         if (WindowState is WindowState.Minimized)
         {
+            FirstPopupWindow.Hide();
+            FirstPopupWindow.Owner = null;
+            FirstPopupWindow.Show();
+
             if (configManager.StopIncreasingTimeAndCharStatsWhenMinimized)
             {
                 StatsUtils.StopTimeStatStopWatch();
@@ -2158,6 +2162,10 @@ internal sealed partial class MainWindow : IDisposable
 
         else
         {
+            FirstPopupWindow.Hide();
+            FirstPopupWindow.Owner = this;
+            FirstPopupWindow.Show();
+
             if (configManager.StopIncreasingTimeAndCharStatsWhenMinimized
                 && (coreConfigManager.CaptureTextFromClipboard || coreConfigManager.CaptureTextFromWebSocket || coreConfigManager.CaptureTextFromTsukikageWebsocket))
             {