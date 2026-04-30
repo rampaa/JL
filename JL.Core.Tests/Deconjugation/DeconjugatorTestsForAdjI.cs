c2563f0adc5eb463fb43a2f922f71f48cae6468c
rampaa <rampaa@waifuland.com>
2026-04-30 18:20:29 +0300

    Add tests for がる

--- JL.Core.Tests/Deconjugation/DeconjugatorTestsForAdjI.cs
+++ JL.Core.Tests/Deconjugation/DeconjugatorTestsForAdjI.cs
@@ -258,4 +258,13 @@ internal sealed class DeconjugatorTestsForAdjI
         string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "良い" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
         Assert.That(actual, Is.EqualTo(expected));
     }
+
+    [Test]
+    public void Deconjugate_Garu_AdjI()
+    {
+        const string termToDeconjugate = "小さがる";
+        const string expected = "～garu";
+        string? actual = LookupResultUtils.DeconjugationProcessesToText(Deconjugator.Deconjugate(termToDeconjugate).Where(static form => form.Text is "小さい" && form.Tags[^1] is "adj-i").Select(static form => form.Process).ToList().AsReadOnlySpan());
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 }
