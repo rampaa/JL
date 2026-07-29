23a0ffe63474c0196c0c9362487f6f66b7661834
rampaa <rampaa@waifuland.com>
2026-07-29 23:18:22 +0300

    Minor

--- JL.Core/Utilities/ObjectPool/PooledList.cs
+++ JL.Core/Utilities/ObjectPool/PooledList.cs
@@ -3,9 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace JL.Core.Utilities.ObjectPool;
 
-internal sealed class PooledList<T>(int initialCapacity) : IEquatable<PooledList<T>>, IDisposable
+internal sealed class PooledList<T>(int initialCapacity) : IDisposable
 {
-    public T[] Array { get; private set; } = ArrayPool<T>.Shared.Rent(initialCapacity);
+    private T[] Array { get; set; } = ArrayPool<T>.Shared.Rent(initialCapacity);
     public int Count { get; private set; }
 
     public void Add(T item)
@@ -34,38 +34,9 @@ internal sealed class PooledList<T>(int initialCapacity) : IEquatable<PooledList
         }
     }
 
-    public Span<T> AsSpan() => Array.AsSpan(0, Count);
+    // public Span<T> AsSpan() => Array.AsSpan(0, Count);
 
     public ReadOnlySpan<T> AsReadOnlySpan() => Array.AsSpan(0, Count);
-
-    public override bool Equals(object? obj)
-    {
-        return obj is PooledList<T> other && Equals(other);
-    }
-
-    public override int GetHashCode()
-    {
-        unchecked
-        {
-            int hash = 17;
-            hash = (hash * 23) + Count.GetHashCode();
-            for (int i = 0; i < Count; i++)
-            {
-                hash *= 23 + (Array[i]?.GetHashCode() ?? 0);
-            }
-            return hash;
-        }
-    }
-
-    public static bool operator ==(PooledList<T> left, PooledList<T> right) => left.Equals(right);
-    public static bool operator !=(PooledList<T> left, PooledList<T> right) => !(left == right);
-
-    public bool Equals(PooledList<T>? other)
-    {
-        return other is not null &&
-            Count == other.Count
-            && Array.AsSpan(0, Count).SequenceEqual(other.Array.AsSpan(0, other.Count));
-    }
 
     public void Dispose()
     {
@@ -87,18 +58,18 @@ internal sealed class PooledList<T>(int initialCapacity) : IEquatable<PooledList
         Count = 0;
     }
 
-    public T this[int index]
-    {
-        get => Array[index];
-        set => Array[index] = value;
-    }
+    // public T this[int index]
+    // {
+    //     get => Array[index];
+    //     set => Array[index] = value;
+    // }
 
-    public void RemoveLastItem()
-    {
-        --Count;
-        if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
-        {
-            Array[Count] = default!;
-        }
-    }
+    // public void RemoveLastItem()
+    // {
+    //     --Count;
+    //     if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
+    //     {
+    //         Array[Count] = default!;
+    //     }
+    // }
 }
--- JL.Core/Utilities/ObjectPool/RentedArrayBuffer.cs
+++ JL.Core/Utilities/ObjectPool/RentedArrayBuffer.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace JL.Core.Utilities.ObjectPool;
 
-internal sealed class RentedArrayBuffer<T>(int capacity) : IEquatable<RentedArrayBuffer<T>>, IDisposable
+internal sealed class RentedArrayBuffer<T>(int capacity) : IDisposable
 {
     private T[] Array { get; } = ArrayPool<T>.Shared.Rent(capacity);
     private int Count { get; set; }
@@ -15,42 +15,14 @@ internal sealed class RentedArrayBuffer<T>(int capacity) : IEquatable<RentedArra
         ++Count;
     }
 
-    public Span<T> AsSpan() => Array.AsSpan(0, Count);
+    // public Span<T> AsSpan() => Array.AsSpan(0, Count);
 
     public ReadOnlySpan<T> AsReadOnlySpan() => Array.AsSpan(0, Count);
-
-    public override bool Equals(object? obj)
-    {
-        return obj is RentedArrayBuffer<T> other && Equals(other);
-    }
-
-    public override int GetHashCode()
-    {
-        unchecked
-        {
-            int hash = 17;
-            hash = (hash * 23) + Count.GetHashCode();
-            for (int i = 0; i < Count; i++)
-            {
-                hash *= 23 + (Array[i]?.GetHashCode() ?? 0);
-            }
-            return hash;
-        }
-    }
-
-    public static bool operator ==(RentedArrayBuffer<T> left, RentedArrayBuffer<T> right) => left.Equals(right);
-    public static bool operator !=(RentedArrayBuffer<T> left, RentedArrayBuffer<T> right) => !(left == right);
-
-    public bool Equals(RentedArrayBuffer<T>? other)
-    {
-        return other is not null &&
-            Count == other.Count
-            && Array.AsSpan(0, Count).SequenceEqual(other.Array.AsSpan(0, other.Count));
-    }
 
     public T this[int index]
     {
         get => Array[index];
+        // ReSharper disable once UnusedMember.Global
         set => Array[index] = value;
     }
 
